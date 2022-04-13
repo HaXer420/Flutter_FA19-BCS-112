a8947d2389c9eae38d9d72b7a1f9b03e6b61aa05
Massab Iftikhar <massabiftkhar@gmail.com>
2022-04-13 20:28:17 +0500

    working

--- bmi_calculator_custom/lib/inputpage.dart
+++ bmi_calculator_custom/lib/inputpage.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'ContainerFile.dart';
+import 'IconTextFile.dart';
+
+const activeColor = Color(0xFF1D1E33);
+const deActiveColor = Color(0xFF111328);
 
 class InputPage extends StatefulWidget {
 
@@ -7,16 +13,89 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleColor = deActiveColor;
+  Color femaleColor = deActiveColor;
+  void updateColor(int gender)
+  {
+    if (gender == 1) {
+      maleColor = activeColor;
+      femaleColor = deActiveColor;
+    }
+    if (gender == 2) {
+      maleColor = deActiveColor;
+      femaleColor = activeColor;
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI Calculator by Massab'),
       ),
-      body: Center(
-        child: Text('Body'),
-      ),
+      body: Column(
+        children: [
+          Expanded(child: Row(
+            children: [
+              Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColor(1);
+                      });
+                    },
+                    child: RepeatContainerCode(
+                      colors: maleColor,
+                      cardWidget: RepeatTextandIconWidget(
+                        iconData: FontAwesomeIcons.male,
+                        label: 'MALE',
+                      ),
+                    ),
+                  ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      updateColor(2);
+                    });
+                  },
+                  child: RepeatContainerCode(
+                  colors: femaleColor,
+                  cardWidget: RepeatTextandIconWidget(
+                    iconData: FontAwesomeIcons.female,
+                    label: 'FEMALE',
+                  ),
 
+              ),
+                ),
+              ),
+            ],
+          ),
+          ),
+          Expanded(child: RepeatContainerCode(
+            colors: Color(0xFF1DE33),
+          ),
+          ),
+          Expanded(child: Row(
+            children: [
+              Expanded(child: RepeatContainerCode(
+                colors: Color(0xFF1DE33),
+              ),
+              ),
+              Expanded(child: RepeatContainerCode(
+                colors: Color(0xFF1DE33),
+              ),
+              ),
+            ],
+          ),
+          ),
+        ],
+      ),
     );
   }
 }
+
+
+
